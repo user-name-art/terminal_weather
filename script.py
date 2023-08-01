b19679101ad61ee07e578687faf38f1bc6277801
Art Gilyazov <art.gilyazov@mail.ru>
2023-08-01 15:44:50 +0300

    add dict with parameters

--- script.py
+++ script.py
@@ -7,14 +7,21 @@ CITIES = [
     'Череповец',
 ]
 
+REQUEST_PARAMETRS = {
+    'nTqu': '',
+    'm': '',
+    'M': '',
+    'lang': 'ru',
+}
 
-def get_weather_by_city(city):
-    url = 'https://wttr.in/{}?nTqu&?m&?M&lang=ru'.format(city)
-    response = requests.get(url)
+
+def get_weather_by_city(city, REQUEST_PARAMETRS):
+    url = 'https://wttr.in/{}'.format(city)
+    response = requests.get(url, params=REQUEST_PARAMETRS)
     response.raise_for_status()
     return response.text
 
 
 if __name__ == '__main__':
     for city in CITIES:
-        print(get_weather_by_city(city))
+        print(get_weather_by_city(city, REQUEST_PARAMETRS))
