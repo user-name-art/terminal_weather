2b8fc6471747255cd81f8c7627e5736b30633860
Art Gilyazov <art.gilyazov@mail.ru>
2023-08-02 08:00:14 +0300

    fix variable name

--- script.py
+++ script.py
@@ -15,9 +15,9 @@ REQUEST_PARAMETRS = {
 }
 
 
-def get_weather_by_city(city, request_parametrs):
+def get_weather_by_city(city, request_options):
     url = 'https://wttr.in/{}'.format(city)
-    response = requests.get(url, params=request_parametrs)
+    response = requests.get(url, params=request_options)
     response.raise_for_status()
     return response.text
 
